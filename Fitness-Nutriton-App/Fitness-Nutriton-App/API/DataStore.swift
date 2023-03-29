d8646074e9930c393e43db5f3f5dd34c6e393b95
Nicki Lee <nol3@duke.edu>
2023-03-29 11:32:57 -0400

    created tab container and datastore file and started on issue #3

---
+++ Fitness-Nutriton-App/Fitness-Nutriton-App/API/DataStore.swift
@@ -0,0 +1,15 @@
+//
+//  DataStore.swift
+//  Fitness-Nutriton-App
+//
+//  Created by Nicki Lee on 3/29/23.
+//
+
+import Foundation
+import SwiftUI
+
+class DataStore: ObservableObject {
+    @Published var userList: [User] = User.previewData
+    
+    
+}
