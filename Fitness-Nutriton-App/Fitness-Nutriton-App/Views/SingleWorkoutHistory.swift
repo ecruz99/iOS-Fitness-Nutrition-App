972c6b59fadf428994f3ee2e1703c8a4f16be0f6
Erik Cruz <erc37@duke.edu>
2023-04-02 19:12:27 -0400

    Workout history ui looks better now

--- Fitness-Nutriton-App/Fitness-Nutriton-App/Views/SingleWorkoutHistory.swift
+++ Fitness-Nutriton-App/Fitness-Nutriton-App/Views/SingleWorkoutHistory.swift
@@ -3,13 +3,52 @@
 import SwiftUI
 
 struct SingleWorkoutHistory: View {
+    let workout: Workout
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            VStack(alignment: .leading){
+                Text(workout.name)
+                    .padding(.bottom, 10)
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                HStack{
+                    Text("Date:")
+                        .fontWeight(.semibold)
+                    Text("\(workout.startedAt.formatted(date: .abbreviated, time: .shortened))")
+                    
+                }
+                .padding(.bottom, 25)
+                ForEach(workout.exercises){exercise in
+                    VStack(alignment: .leading){
+                        Text(exercise.name)
+                            .font(.title3)
+                            .fontWeight(.medium)
+                            .padding(.bottom, 0.5)
+                        ForEach(exercise.activities){activity in
+                            HStack{
+                                Text("Set: \(exercise.activities.firstIndex(where: {$0.id == activity.id})! + 1)")
+                                Spacer()
+                                Text("Weight: \(activity.weight)")
+                                Spacer()
+                                Text("Reps: \(activity.reps)")
+                            }
+                        }
+                    }
+                    .padding(.bottom)
+                }
+                
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(10)
+            .background(RoundedRectangle(cornerRadius: 10).fill(Color.gray.opacity(0.15)))
+            .padding()
+            
+        }
     }
 }
 
 struct SingleWorkoutHistory_Previews: PreviewProvider {
     static var previews: some View {
-        SingleWorkoutHistory()
+        SingleWorkoutHistory(workout: Workout.previewData[0])
     }
 }
--- Fitness-Nutriton-App/Fitness-Nutriton-App/Views/WorkoutHistory.swift
+++ Fitness-Nutriton-App/Fitness-Nutriton-App/Views/WorkoutHistory.swift
@@ -18,39 +18,7 @@ struct WorkoutHistory: View {
                 .fontWeight(.bold)
                 .padding(.top, 10)
             ForEach(Workout.reverseSort(dataStore.workouts)) { workout in
-                VStack(alignment: .leading){
-                    Text(workout.name)
-                        .padding(.bottom, 1)
-                        .font(.title2)
-                        .fontWeight(.semibold)
-                    HStack{
-                        Text("Date:")
-                            .fontWeight(.semibold)
-                        Text("\(workout.startedAt.formatted(date: .abbreviated, time: .shortened))")
-
-                    }
-                    .padding(.bottom, 2)
-                    ForEach(workout.exercises){exercise in
-                        VStack(alignment: .leading){
-                            Text(exercise.name)
-                                .font(.title3)
-                            ForEach(exercise.activities){activity in
-                                HStack{
-                                    Text("Set: \(exercise.activities.firstIndex(where: {$0.id == activity.id})! + 1)")
-                                    Text("Weight: \(activity.weight)")
-                                    Text("Reps: \(activity.reps)")
-                                }
-                            }
-                        }
-                        .padding(.bottom)
-                    }
-                    
-                }
-                .padding()
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(.black, lineWidth: 2))
-                
+                SingleWorkoutHistory(workout: workout)
             }
         }
     }
