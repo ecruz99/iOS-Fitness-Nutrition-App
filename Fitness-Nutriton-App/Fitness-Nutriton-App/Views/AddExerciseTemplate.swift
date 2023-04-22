7ec117d7954bd4502a608d261a5d583fa7b9d4a3
Erik Cruz <erc37@duke.edu>
2023-04-22 05:22:33 -0400

    inserted api stuff for exercise template new/edit

--- Fitness-Nutriton-App/Fitness-Nutriton-App/Views/AddExerciseTemplate.swift
+++ Fitness-Nutriton-App/Fitness-Nutriton-App/Views/AddExerciseTemplate.swift
@@ -12,6 +12,10 @@ struct AddExerciseTemplate: View {
     @Binding var data: ExerciseTemplate.FormData
     @State var apiExerciseTemplate: Bool = false
     
+    @EnvironmentObject var exerciseLoader: ExerciseLoader
+    @State var searchText = ""
+    @State var muscleSelection = "Name"
+    var apiSelection = ["Name", "Muscle"]
     
     var body: some View {
         ScrollView {
@@ -26,7 +30,50 @@ struct AddExerciseTemplate: View {
                 }.buttonStyle(.bordered).tint(apiExerciseTemplate ? .blue : .gray)
             }.padding(.bottom, 20)
             if (apiExerciseTemplate) {
-                Text("API results here. All exercises displayed alphabetically, with a search bar at top to search by either name or muscle that filters the list. Clicking on exercise prompts user to 'add', user confirms")
+                ScrollView{
+                    TextFieldWithLabel(label: "Search For Exercise", text: $searchText, prompt: "Search Name")
+                        .padding(.leading)
+                    
+                    Picker("API Search", selection: $muscleSelection) {
+                        ForEach(apiSelection, id: \.self) {
+                            Text($0)
+                        }
+                    }.pickerStyle(.segmented)
+                    
+                    Button("Search") {
+                        Task {
+                            
+                            if muscleSelection == "Name" {
+                                await exerciseLoader.loadExercisesByName(name: searchText)
+                            } else {
+                                await exerciseLoader.loadExercisesByMuscle(muscle: searchText)
+                            }
+                            
+                        }
+                        
+                    }
+                    
+                    
+                    Text("API Results Here")
+                        .padding()
+                    switch exerciseLoader.state {
+                    case .idle: Color.clear
+                    case .loading: ProgressView()
+                    case .failed(let error): Text(error.localizedDescription)
+                    case .success(let apiResponse):
+                        ForEach(apiResponse, id: \.self) { exercise in
+                            HStack {
+                                Text(exercise.name)
+                                Text(" : ")
+                                Text(exercise.muscle)
+                            }
+                        }
+                    }
+                    Text("Clicking 'add' will autofill exercise name and muscle with that api results info")
+                        .padding()
+                    
+
+                }
             } else {
                 ExerciseTemplateForm(data: $data)
                     .padding(.top, 50)
@@ -37,5 +84,6 @@ struct AddExerciseTemplate: View {
 struct AddExerciseTemplate_Previews: PreviewProvider {
     static var previews: some View {
         AddExerciseTemplate(data: Binding.constant(ExerciseTemplate.FormData())).environmentObject(DataStore())
+            .environmentObject(ExerciseLoader(apiClient: ExerciseAPIService()))
     }
 }
--- Fitness-Nutriton-App/Fitness-Nutriton-App/Views/EditExerciseTemplate.swift
+++ Fitness-Nutriton-App/Fitness-Nutriton-App/Views/EditExerciseTemplate.swift
@@ -11,7 +11,12 @@ struct EditExerciseTemplate: View {
     @EnvironmentObject var dataStore: DataStore
     @Binding var data: ExerciseTemplate.FormData
     @State var apiExerciseTemplate: Bool = false
-    @State var searchTerm: String = ""
+    
+    @EnvironmentObject var exerciseLoader: ExerciseLoader
+    @State var searchText = ""
+    @State var muscleSelection = "Name"
+    var apiSelection = ["Name", "Muscle"]
+    
     var body: some View {
         ScrollView {
             Spacer(minLength: 40)
@@ -25,7 +30,50 @@ struct EditExerciseTemplate: View {
                 }.buttonStyle(.bordered).tint(apiExerciseTemplate ? .blue : .gray)
             }.padding(.bottom, 20)
             if (apiExerciseTemplate) {
-                Text("API results here. All exercises displayed alphabetically, with a search bar at top to search by either name or muscle that filters the list. Clicking on exercise prompts user to 'add', user confirms")
+                ScrollView{
+                    TextFieldWithLabel(label: "Search For Exercise", text: $searchText, prompt: "Search Name")
+                        .padding(.leading)
+                    
+                    Picker("API Search", selection: $muscleSelection) {
+                        ForEach(apiSelection, id: \.self) {
+                            Text($0)
+                        }
+                    }.pickerStyle(.segmented)
+                    
+                    Button("Search") {
+                        Task {
+                            
+                            if muscleSelection == "Name" {
+                                await exerciseLoader.loadExercisesByName(name: searchText)
+                            } else {
+                                await exerciseLoader.loadExercisesByMuscle(muscle: searchText)
+                            }
+                            
+                        }
+                        
+                    }
+                    
+                    
+                    Text("API Results Here")
+                        .padding()
+                    switch exerciseLoader.state {
+                    case .idle: Color.clear
+                    case .loading: ProgressView()
+                    case .failed(let error): Text(error.localizedDescription)
+                    case .success(let apiResponse):
+                        ForEach(apiResponse, id: \.self) { exercise in
+                            HStack {
+                                Text(exercise.name)
+                                Text(" : ")
+                                Text(exercise.muscle)
+                            }
+                        }
+                    }
+                    Text("Clicking 'add' will autofill exercise name and muscle with that api results info")
+                        .padding()
+                    
+
+                }
             } else {
                 ExerciseTemplateForm(data: $data)
                     .padding(.top, 50)
@@ -36,5 +84,6 @@ struct EditExerciseTemplate: View {
 struct EditExerciseTemplate_Previews: PreviewProvider {
     static var previews: some View {
         EditExerciseTemplate(data: Binding.constant(ExerciseTemplate.FormData())).environmentObject(DataStore())
+            .environmentObject(ExerciseLoader(apiClient: ExerciseAPIService()))
     }
 }
